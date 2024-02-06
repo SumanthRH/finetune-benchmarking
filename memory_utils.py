bb6fbb7f679844d688e78c137c960acefdab4f40
SumanthRH <sumanthrh99@gmail.com>
2024-02-05 17:14:31 -0800

    x

--- memory_utils.py
+++ memory_utils.py
@@ -1,11 +1,14 @@
 from transformers import AutoConfig
 
-def get_activation_memory(model_config: AutoConfig, num_bytes: int =4, batch_size: int = 1, sequence_length:int = 512):
+def get_activation_memory(model_config: AutoConfig, num_bytes: int =4, batch_size: int = 1, sequence_length:int = 512, grad_checkpointing=False):
     b = batch_size
     N = num_bytes
     L = model_config.num_hidden_layers
     h = model_config.hidden_size
     a = model_config.num_attention_heads
     s = sequence_length
-    activation_mem = L*((16*N+2)*s*b*h + (2*N+1)*a*s*s*b)
+    if grad_checkpointing:
+        activation_mem = (16*N+2)*s*b*h + (2*N+1)*a*s*s*b + N*s*b*h # add temp memory + checkpointed mem
+    else:
+        activation_mem = L*((16*N+2)*s*b*h + (2*N+1)*a*s*s*b)
     return activation_mem//(1024**2)
